d94d53d8bae81ee57363677944b80040de727dde
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-08-14 17:02:11 +0430

    authentication of backend added using devise-jw

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,27 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    if user_exists?(sign_up_params[:email])
+      render json: { error: "Email already exists. Please choose a different email!"}, status: :unprocessable_entity
+    else
+      build_resource(sign_up_params)
+      if resource.save
+        sign_in(resource_name, resource)
+        render json: resource
+      else
+        render json: { error: "'Failed to create user. Check on input!"}, status: :unprocessable_entity
+      end
+    end
+  end
+
+  protected
+
+  def user_exists?(email)
+    User.exists?(email:)
+  end
+  
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: "", path_names: {
+    sign_in: "/login",
+    sign_out: '/logout',
+    registration: "/signup",
+    password: '/reset_password'
+  }, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
 end
