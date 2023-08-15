8b5f9d1d9fc53a38123cdd58a7eeef29cd846805
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-08-15 18:15:05 +0430

    assoications and validations added for the models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,8 @@
 class Doctor < ApplicationRecord
+    belongs_to :user
+    has_many :appointments, dependent: :destroy
+
+    validates :name, presence: true
+    validates :bio, presence: true
+    validates :image, presence: true
 end
