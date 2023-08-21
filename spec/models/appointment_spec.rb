b22eeca10ea5d5a03535babd4e89b5e881bb1fe5
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-08-21 22:00:50 +0530

    Linters fixed

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Appointment, type: :model do
   describe 'Appointment model' do
     subject do
-      Appointment.new(city: 'nairobi', date: 2023 , doctor_id: 1)
+      Appointment.new(city: 'nairobi', date: 2023, doctor_id: 1)
     end
 
     before { subject.save }
     it 'check the date is not blank' do
       subject.date = nil
       expect(subject).to_not be_valid
-    end    
+    end
 
     describe 'Associations' do
       it 'has many users' do
